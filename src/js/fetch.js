457def57c544f1462f1fb52fc2af4e419c177957
MateuszBieda <mateusbieda@gmail.com>
2023-04-18 21:51:42 +0200

    format_update

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,51 +1,52 @@
 //import axios from "axios";
-export {getMoviesbyKeyword};
+export { getMoviesbyKeyword };
 
 //axios.defaults.baseURL = `https://api.themoviedb.org/3/search/keyword?api_key=${APIkey}`;
 
 //const trendingMoviesDOM = document.querySelector('.movie-list');
-const APIkey= "ac3e035161883f7175e5be9954a0068d";
-let keyword ="";
-let page =1; 
+const APIkey = 'ac3e035161883f7175e5be9954a0068d';
+let keyword = '';
+let page = 1;
 
-const gallery = document.querySelector(".movie-list");
-const form = document.querySelector("#header__form");
+const gallery = document.querySelector('.movie-list');
+const form = document.querySelector('#header__form');
 //const input =document.querySelector(".header__input");
-form.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
- async function getMoviesbyKeyword()  {
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APIkey}&query=${keyword}&page=${page}`);
-    const data = response.json();
-    return data; 
-  }
+async function getMoviesbyKeyword() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${APIkey}&query=${keyword}&page=${page}`,
+  );
+  const data = response.json();
+  return data;
+}
 
-  getMoviesbyKeyword()
+getMoviesbyKeyword()
   .then(data => console.log(data.results))
   .catch(error => console.log(error.message));
 
+function handleSubmit(e) {
+  e.preventDefault();
+  //keyword=input.value.trim();
+  const APIkey = 'ac3e035161883f7175e5be9954a0068d';
+  keyword = e.currentTarget.name.value.trim();
+  console.log(keyword);
+  gallery.innerHTML = '';
 
-function handleSubmit(e){
-    e.preventDefault();
-    //keyword=input.value.trim();
-    const APIkey= "ac3e035161883f7175e5be9954a0068d";
-    keyword=e.currentTarget.name.value.trim();
-    console.log(keyword);
-    gallery.innerHTML='';
-    
-    getMoviesbyKeyword(keyword)
-  .then((data) => {
-    renderMoviesList(data)
-})
-  .catch(error => console.log(error.message));
-    //console.log(keyword);
+  getMoviesbyKeyword(keyword)
+    .then(data => {
+      renderMoviesList(data);
+    })
+    .catch(error => console.log(error.message));
+  //console.log(keyword);
 }
 
-
-function renderMoviesList(data){
-    const URLimage= "https://image.tmdb.org/t/p/original"
-    const markup = data.results
-    .map((key) =>
-    ` <li>
+function renderMoviesList(data) {
+  const URLimage = 'https://image.tmdb.org/t/p/original';
+  const markup = data.results
+    .map(
+      key =>
+        ` <li>
         <div class="movie-card">
             <img class="movie-card__img" src="${URLimage}${key.poster_path}" loading="lazy" 
             />
@@ -55,14 +56,14 @@ function renderMoviesList(data){
             </div>
         </div>
         </li>
-        `)
-   .join("");
-    
-   return gallery.insertAdjacentHTML('beforeend',markup);
+        `,
+    )
+    .join('');
 
-}  
+  return gallery.insertAdjacentHTML('beforeend', markup);
+}
 
-  // async function getMoviesbyKeyword (keyword) {
+// async function getMoviesbyKeyword (keyword) {
 //     const response = await axios.get(
 //       `&query=${keyword}`,
 //     );
