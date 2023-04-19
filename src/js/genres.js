611259e708dc7919fce86325d2f0125faa6c78b5
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-04-19 18:02:59 +0200

    Genres
    1. plik pomocniczy genres.js - przekazanie wszystkich genres do localStorage
    2. trending - uzupełnienie kart filmów o genres

--- src/js/genres.js
+++ src/js/genres.js
@@ -1,42 +1,24 @@
-// const getGenres = async() => {
-//     const APIkey = 'ac3e035161883f7175e5be9954a0068d';
-//     const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIkey}`;
-//     try {
-//         const genresData = await fetch(url); 
-//         const genresDataJSON = genresData.json();
-//         return genresDataJSON;
-//     }
-//     catch (error) {
-//         console.log(error.message)
-//     }
-// }
+const getAllGenres = async() => {
+    const APIkey = 'ac3e035161883f7175e5be9954a0068d';
+    const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIkey}`;
+    try {
+        const genresData = await fetch(url); 
+        const genresDataJSON = await genresData.json();        
+        return genresDataJSON;
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+}
+
+getAllGenres()
+.then((genresDataJSON) => movieGenres(genresDataJSON))
+.catch((error) => console.log(error))
 
 
+const movieGenres = (genresDataJSON) => {
+    const genres = genresDataJSON.genres;
+    localStorage.setItem('genres', JSON.stringify(genres));
+}
 
-// getGenres()
-// .then((genresDataJSON) => console.log(genresDataJSON.genres))
-// .catch((error) => console.log(error))
-
-// export default getGenres;
-
-const genres = [
-    {"id":28,"name":"Action"},
-    {"id":12,"name":"Adventure"},
-    {"id":16,"name":"Animation"},
-    {"id":35,"name":"Comedy"},
-    {"id":80,"name":"Crime"},
-    {"id":99,"name":"Documentary"},
-    {"id":18,"name":"Drama"},
-    {"id":10751,"name":"Family"},
-    {"id":14,"name":"Fantasy"},
-    {"id":36,"name":"History"},
-    {"id":27,"name":"Horror"},{"id":10402,"name":"Music"},
-    {"id":9648,"name":"Mystery"},
-    {"id":10749,"name":"Romance"},
-    {"id":878,"name":"Science Fiction"},
-    {"id":10770,"name":"TV Movie"},
-    {"id":53,"name":"Thriller"},
-    {"id":10752,"name":"War"},
-    {"id":37,"name":"Western"}];
-
-    export default genres;
+export default getAllGenres;
