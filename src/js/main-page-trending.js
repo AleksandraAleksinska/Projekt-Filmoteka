7c44cabf84acbcee891311a93d23e620e61514c1
karollaski <karol.laski994@gmail.com>
2023-04-19 21:58:55 +0200

    added pagination trending movies
    Dodałem paginację dla trending movies w wersji surowej css, ale strony się zmieniają.
    Pozwoliłem sobie dodać w module main-page-trending.js -> getTrendingMovies jako parametr (page =1) i do linku dodałem &page=${page}.

--- src/js/main-page-trending.js
+++ src/js/main-page-trending.js
@@ -1,27 +1,25 @@
-
 const trendingMoviesDOM = document.querySelector('.movie-list');
 
-const getTrendingMovies = async () => {
-    const APIkey = 'ac3e035161883f7175e5be9954a0068d';
-    const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${APIkey}`;
-    try{
-        const response = await fetch(url);
-        const responseJSON = await response.json();
-        console.log(responseJSON);
-        return responseJSON;
-        
-    } catch (error) {
-        console.log(error.message)
-    }     
-}
+const getTrendingMovies = async (page = 1) => {
+  const APIkey = 'ac3e035161883f7175e5be9954a0068d';
+  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${APIkey}&page=${page}`;
+  try {
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+    return responseJSON;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const renderTrendingMovies = (response) => {
-    const movies = response.results; 
-    const imgUrl = 'https://image.tmdb.org/t/p/w500';  
-    const markup = (movies)    
-    .map(({poster_path, title, release_date}) => {
-        const realeseYear = release_date.slice(0, 4);
-        return `
+const renderTrendingMovies = response => {
+  const movies = response.results;
+  const imgUrl = 'https://image.tmdb.org/t/p/w500';
+  const markup = movies
+    .map(({ poster_path, title, release_date }) => {
+      const realeseYear = release_date.slice(0, 4);
+      return `
         <li>
         <div class="movie-card">
             <img class="movie-card__img" src="${imgUrl}${poster_path}" loading="lazy" 
@@ -32,16 +30,15 @@ const renderTrendingMovies = (response) => {
             </div>
         </div>
         </li>
-        `
+        `;
     })
     .join('');
-    trendingMoviesDOM.innerHTML=markup;
-
-}
+  trendingMoviesDOM.innerHTML = markup;
+};
 
 getTrendingMovies()
-.then((movies) => renderTrendingMovies(movies))
-.catch((error) => console.log(error));
+  .then(movies => renderTrendingMovies(movies))
+  .catch(error => console.log(error));
 
-const trendingMovies = {getTrendingMovies, renderTrendingMovies};
+const trendingMovies = { getTrendingMovies, renderTrendingMovies };
 export default trendingMovies;
---
+++ src/js/pagination-trending-movies.js
@@ -0,0 +1,39 @@
+import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
+import trendingMovies from './main-page-trending';
+
+const VISIBEL_PAGES = 5;
+const ITEM_PER_PAGE = 20;
+
+const setPagination = totalItems => {
+  const options = {
+    totalItems,
+    itemsPerPage: ITEM_PER_PAGE,
+    visiblePages: VISIBEL_PAGES,
+    centerAlign: true,
+  };
+
+  const pagination = new Pagination('pagination', options);
+
+  return pagination;
+};
+
+const getPagination = async () => {
+  try {
+    const data = await trendingMovies.getTrendingMovies();
+    const pagination = setPagination(data.total_results);
+
+    pagination.on('afterMove', event => {
+      const currentPage = event.page;
+      console.log(currentPage);
+
+      trendingMovies
+        .getTrendingMovies(currentPage)
+        .then(data => trendingMovies.renderTrendingMovies(data))
+        .catch(error => console.log(error));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+getPagination();
