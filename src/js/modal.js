fdebbc109f1f473904c2bc1f539eff2cf5f4acd8
Bartek <Barteknr1@gmail.com>
2023-04-20 19:17:35 +0200

    Improved modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -17,21 +17,25 @@ const renderSelectedMovieDetails = (movie) => {
     const imgUrl = 'https://image.tmdb.org/t/p/w500';
     const selectedMovie =
         `<img class="modal__img" src="${imgUrl}${movie.poster_path}"></img>
-        <h2 class="modal__title"> ${movie.title}</>
-        <div class="modal__info-wrapper">
+        <h2 class="modal__title uppercase"> ${movie.title}</h2>
             <ul class="modal__info">
-                <li class="modal__info modal__info--key">Vote / Votes</li>
-                <li class="modal__info modal__info--key">Popularity</li>
-                <li class="modal__info modal__info--key">Original Title</li>
-                <li class="modal__info modal__info--key">Genre</li>
+                <li class="modal__info-item">
+                    <p class="modal__info--key">Vote / Votes</p>
+                    <p class="modal__info--number">${movie.vote_average.toFixed(1)} / ${movie.vote_count}</p>
+                    </li>
+                <li class="modal__info-item">
+                    <p class="modal__info--key">Popularity</p>
+                    <p class="modal__info--number">${movie.popularity.toFixed(1)}</p>
+                    </li>
+                <li class="modal__info-item">
+                   <p class="modal__info--key">Original Title</p>
+                   <p class="modal__info--value uppercase">${movie.original_title}</p>
+                   </li>
+                <li class="modal__info-item">
+                   <p class="modal__info--key">Genre</p>
+                   <p class="modal__info--value">${movie.genres.map(genre => genre.name).join(', ')}</p>
+                   </li>
             </ul>
-            <ul class="modal__info">
-                <li class="modal__info modal__info--number">${movie.vote_average.toFixed(1)} / ${movie.vote_count}</li>
-                <li class="modal__info modal__info--number">${movie.popularity.toFixed(1)}</li>
-                <li class="modal__info modal__info--value">${movie.original_title}</li>
-                <li class="modal__info modal__info--value">${movie.genres.map(genre => genre.name).join(', ')}</li>
-            </ul>
-        </div>
         <div class="modal__overview">
             <p class="modal__overview--about">about</p>
             <p>${movie.overview}</p>
