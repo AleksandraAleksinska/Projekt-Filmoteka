a746603277543e0afa66c6f3c00adbd66d380f69
MateuszBieda <mateusbieda@gmail.com>
2023-04-21 23:05:41 +0200

    duplicated import delated from index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../sass/main.scss';
 
 import trendingMovies from './main-page-trending';
-import {getMoviesbyKeyword} from "./fetch.js"
+
 import selectedMovie from './modal';
 
 import trendingMovies from './main-page-trending';
