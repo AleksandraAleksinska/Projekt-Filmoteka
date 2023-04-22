b5a3be901bae38cf2e6de17bc7c498dbb599f72d
Andrzej Michalski <113372492+Michalski84@users.noreply.github.com>
2023-04-22 23:18:48 +0200

    feature/library

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,4 @@ import trendingMovies from './main-page-trending';
 import { getMoviesbyKeyword } from './fetch.js';
 import selectedMovie from './modal';
 import pagination from './pagination';
+import library from './library.js';
--- src/js/library.js
+++ src/js/library.js
@@ -1,45 +1,66 @@
-const renderMoviesList = (movies, listElement) => {
-  const imgUrl = 'https://image.tmdb.org/t/p/w500';
+const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+const queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
 
-  const moviesList = movies.reduce((acc, movie) => {
-    const { id, title, release_date, poster_path, genre_ids } = movie;
-    const releaseYear = new Date(release_date).getFullYear();
-    const movieGenres = genre_ids.map(genreId => genres.find(genre => genre.id === genreId)?.name);
+const movies = [];
 
-    acc += `
-      <li>
-        <div class="movie-card">
-          <img class="movie-card__img" src="${imgUrl}${poster_path}" loading="lazy"/>
-          <div class="movie-card__desc">
-            <p class="movie-card__title">${title}</p>
-            <p class="movie-card__info">${movieGenres.slice(0, 3).join(', ')} | ${releaseYear}</p>
-          </div>
-        </div>
-      </li>
-    `;
-    return acc;
-  }, '');
-
-  listElement.innerHTML = moviesList;
-};
-
-const renderWatchedMovies = () => {
-  const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
-  renderMoviesList(watchedMovies, document.querySelector('#library__watched ul'));
-};
-
-const renderQueuedMovies = () => {
-  const queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
-  renderMoviesList(queuedMovies, document.querySelector('#library__queue ul'));
-};
-
-document
-  .querySelector('.header__libBtn.btn-watched')
-  .addEventListener('click', renderWatchedMovies);
-document.querySelector('.header__libBtn.btn-queue').addEventListener('click', renderQueuedMovies);
-
-const watchedButton = document.querySelector('#watched-button');
-watchedButton.addEventListener('click', () => {
-  console.log('Watched button clicked');
-  // Tutaj można dodać kod do pobrania filmów z localStorage i wygenerowania ich kafelków
+watchedMovies.forEach(movie => {
+  const { id, title, poster_path, release_date, genres } = movie;
+  movies.push({ id, title, status: 'watched', poster_path, release_date, genres });
 });
+
+queuedMovies.forEach(movie => {
+  const { id, title, poster_path, release_date, genres } = movie;
+  movies.push({ id, title, status: 'queued', poster_path, release_date, genres });
+});
+
+console.log(movies);
+
+const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
+const imgUrl = 'https://image.tmdb.org/t/p/w500/';
+
+const generateMovieCardHTML = movie => {
+  const { title, poster_path, release_date, genres } = movie;
+  const releaseYear = new Date(release_date).getFullYear();
+  const movieGenres = genres.map(genre => genre.name);
+
+  return `
+    <li>
+      <div class="movie-card">
+        <img class="movie-card__img" src="${imgUrl}${poster_path}" loading="lazy"/>
+        <div class="movie-card__desc">
+          <p class="movie-card__title">${title}</p>
+          <p class="movie-card__info">${movieGenres.slice(0, 3).join(', ')} | ${releaseYear}</p>
+        </div>
+      </div>
+    </li>
+  `;
+};
+
+const fetchMovieData = async movieId => {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data;
+};
+
+const watchedBtn = document.querySelector('.btn-watched');
+const queueBtn = document.querySelector('.btn-queue');
+const movieList = document.querySelector('.movie-list');
+
+watchedBtn.addEventListener('click', () => {
+  const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+  movieList.innerHTML = watchedMovies.map(movie => generateMovieCardHTML(movie)).join('');
+});
+
+queueBtn.addEventListener('click', async () => {
+  const queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
+  const movieDataArray = await Promise.all(queuedMovies.map(movie => fetchMovieData(movie.id)));
+  const moviesWithDetails = queuedMovies.map((movie, index) => ({
+    ...movie,
+    ...movieDataArray[index],
+  }));
+  movieList.innerHTML = moviesWithDetails.map(movie => generateMovieCardHTML(movie)).join('');
+});
+
+const library = { generateMovieCardHTML, fetchMovieData, movies };
+
+export default library;
