c13908b451700c873061f88ca670f5f7f72a6a8e
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2023-04-23 17:54:29 +0200

    local-storage__queue update(4)

--- src/js/local-storage__queue.js
+++ src/js/local-storage__queue.js
@@ -1,18 +1,47 @@
 const queueButton = document.querySelector('.button--queue');
 const userQueue = document.querySelector('.btn-queue');
+const queueContainer = document.querySelector('.queue-container');
+const queueList = document.querySelector('.queue-list');
 
-queueButton.addEventListener('click', () => {
-  localStorage.setItem('queueElements', '');
-});
-
-userQueue.addEventListener('click', () => {
-  localStorage.getItem('queueElements');
-});
-
-const addToQueue = async () => {
+const fetchMovies = async () => {
   try {
-    // const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
+    const movieId = '';
+    const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
+    const movieToQueue = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`,
+    );
+    const movieToQueueJSON = await movieToQueue.json();
+    console.log(movieToQueueJSON);
+    return movieToQueueJSON;
   } catch (error) {
     console.log(error.message);
   }
 };
+
+const addToQueue = movie => {
+  const imgUrl = 'https://image.tmdb.org/t/p/w500';
+  const usersMovie = `
+  <div>
+      <img src="${imgUrl}${movie.poster_path}" loading="lazy" 
+      />
+      <div>
+      <p>${movie.title}</p>
+      <p> ${movie.genre_ids
+        .map(genre => genre.name)
+        .slice(0, 3)
+        .join(', ')} | ${movie.release_date}</p>                   
+      </div>
+  </div>`;
+  queueList.innerHTML = usersMovie;
+};
+
+// queueButton.addEventListener('click', () => {
+//   localStorage.setItem('queueElements', addToQueue);
+// });
+
+// userQueue.addEventListener('click', () => {
+//   const queueElement = localStorage.getItem('queueElements');
+//   const queueItem = document.createElement('li');
+//   queueItem.textContent = queueElement;
+//   queueList.appendChild(queueItem);
+// });
