4d35f9cbcfbc57d0650fa14db8c094147d8a18c6
MateuszBieda <mateusbieda@gmail.com>
2023-04-23 22:51:55 +0200

    fetch keyword :add pagination

--- src/js/fetchMoviesByKeyword.js
+++ src/js/fetchMoviesByKeyword.js
@@ -1,4 +1,8 @@
+
 export { getMoviesbyKeyword };
+
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 
 const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
 let keyword = '';
@@ -9,11 +13,9 @@ const qs = s => document.querySelector(s);
 const gallery = qs('.movie-list');
 const form = qs('#header__form');
 
-form.addEventListener('submit', handleSubmitKeyword);
-
-async function getMoviesbyKeyword() {
+async function getMoviesbyKeyword(keyword,page=1) {
   const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${keyword}&page=${page}`,
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${keyword}&page=${page}&language=en-US`,
   );
   const data = response.json();
   return data;
@@ -23,18 +25,7 @@ getMoviesbyKeyword()
   .then(data => console.log(data.results))
   .catch(({ message }) => console.log(message));
 
-const handleSubmitKeyword = e => {
-  e.preventDefault();
-  const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
-  keyword = e.currentTarget.name.value.trim();
-  gallery.innerHTML = '';
-
-  return getMoviesbyKeyword(keyword)
-    .then(data => {
-      renderMoviesList(data);
-    })
-    .catch(({ message }) => console.log(message));
-};
+form.addEventListener('submit', handleSubmitKeyword);
 
 const renderMoviesList = data => {
   const IMAGE_URL = 'https://image.tmdb.org/t/p/original';
@@ -54,6 +45,35 @@ const renderMoviesList = data => {
         `,
     )
     .join('');
-
+      
   gallery.insertAdjacentHTML('beforeend', markup);
 };
+
+
+function handleSubmitKeyword(e) {
+  e.preventDefault();
+  keyword = e.currentTarget.name.value.trim();
+  console.log(keyword);
+  gallery.innerHTML = '';
+  getMoviesbyKeyword(keyword)
+    .then(data => {
+      const pagination = new Pagination('pagination', {
+        totalItems: data.total_results,
+        itemsPerPage: 20,
+        visiblePages: 5,
+        centerAlign: true,
+        currentPage: page,
+      });
+      pagination.on('beforeMove', event => {
+        const currentPage = event.page;
+        getMoviesbyKeyword(keyword, currentPage)
+          .then(data => {
+            gallery.innerHTML = '';
+            renderMoviesList(data);
+          })
+          .catch(error => console.log(error));
+      });
+      renderMoviesList(data);
+    })
+    .catch(error => console.log(error.message));
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../sass/main.scss';
 
 import trendingMovies from './main-page-trending';
-import {getMoviesbyKeyword} from "./fetch.js"
+import {getMoviesbyKeyword} from "./fetchMoviesByKeyword.js"
 import selectedMovie from './modal';
 
 import trendingMovies from './main-page-trending';
