3cd99e38f05f84e55b88e10c880bdfa40c083541
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-04-24 23:45:09 +0200

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../sass/main.scss';
 
 import trendingMovies from './main-page-trending';
-import { getMoviesbyKeyword } from './fetch.js';
+import { getMoviesbyKeyword } from './fetchMoviesByKeyword';
 import selectedMovie from './modal';
 import pagination from './pagination';
 import toggleModal from './toggleModal';
