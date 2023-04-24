faa953c456972044648f0544d5885d0375af8f77
Bartek <Barteknr1@gmail.com>
2023-04-24 16:35:17 +0200

    Added close functions

--- src/js/toggleModal.js
+++ src/js/toggleModal.js
@@ -1,16 +1,28 @@
 const modalSettings = (() => {
-    const refs = {
+  const refs = {
       
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
   
     
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
   
-    function toggleModal() {
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("backdrop")) {
       refs.modal.classList.toggle("is-hidden");
     }
-  })();
+  });
+
+  document.addEventListener("keydown", e => {
+    if (e.code === "Escape") {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  });
+  
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
   
   export default modalSettings
