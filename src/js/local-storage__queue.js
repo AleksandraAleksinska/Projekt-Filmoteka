73ffc44e4246c55e40d03cf9cd375822713861df
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-04-24 23:11:44 +0200

    Update local-storage__queue.js

--- src/js/local-storage__queue.js
+++ src/js/local-storage__queue.js
@@ -1,29 +1,12 @@
 import selectedMovie from "./modal";
 
 const queueMoviesDOM = document.querySelector('.users-queue-list');
-const savedMovie = localStorage.getItem('queue-movie');
-const parsedMovie = JSON.parse(savedMovie);
-const queueMovieId = parsedMovie.id;
-console.log(queueMovieId)
+const savedMovies = localStorage.getItem('queue-movie');
+const parsedMovies = JSON.parse(savedMovies);
+// const queueMovieId = parsedMovies.id;
+console.log(parsedMovies)
 
-const getQueueMovie = async (page = 1) => {
-  const APIkey = 'ac3e035161883f7175e5be9954a0068d';
-  const url = `https://api.themoviedb.org/3/movie/${queueMovieId}?api_key=${APIkey}&page=${page}`;
-  try {
-    const movieData = await fetch(url);
-    const movieDataJSON = await movieData.json();
-    console.log(movieDataJSON)
-    return movieDataJSON;
-    
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-getQueueMovie()
-  .then(movie => renderQueueMovie(movie))
-  .catch(error => console.log(error));
-
-const renderQueueMovie = (movie) => {
+const renderQueueMovie = parsedMovies.forEach((movie) => {
   
   const imgUrl = 'https://image.tmdb.org/t/p/w500';
   console.log(movie);
@@ -41,8 +24,10 @@ const renderQueueMovie = (movie) => {
         </li>
         `;
     
+    if (queueMoviesDOM) {
+      queueMoviesDOM.insertAdjacentHTML('beforeend', markup);
+    }
     
-    queueMoviesDOM.insertAdjacentHTML('beforeend', markup);
     const movieList = document.querySelectorAll('li');
      movieList.forEach(movieListItem => {
 
@@ -63,96 +48,24 @@ const renderQueueMovie = (movie) => {
     const libBtnQueue = document.querySelector('.active-button');
     const libBtnWatched = document.querySelector('.btn-watched');
 
-    libBtnWatched.addEventListener('click', () => {
-    queueMoviesDOM.classList.add('d-none');
-    libBtnWatched.classList.add('active-button');
-    libBtnQueue.classList.remove('active-button'); 
+    if (queueMoviesDOM) {
 
-    })
-    libBtnQueue.addEventListener('click', () => {
-    queueMoviesDOM.classList.remove('d-none');
-    libBtnWatched.classList.remove('active-button');
-    libBtnQueue.classList.add('active-button'); 
-
-    })
-
-    const btnAddToWatched = document.querySelector('.button--watched');
-    btnAddToWatched.addEventListener('click', () => {
-        
-        
-      })
-
-  }
-   
+      libBtnWatched.addEventListener('click', () => {
+        queueMoviesDOM.classList.add('d-none');
+        libBtnWatched.classList.add('active-button');
+        libBtnQueue.classList.remove('active-button'); 
+    
+        })
+        libBtnQueue.addEventListener('click', () => {
+        queueMoviesDOM.classList.remove('d-none');
+        libBtnWatched.classList.remove('active-button');
+        libBtnQueue.classList.add('active-button'); 
+    
+        })   
+            
 
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const queueButton = document.querySelector('.button--queue');
-// const userQueue = document.querySelector('.btn-queue');
-// const queueContainer = document.querySelector('.queue-container');
-// const queueList = document.querySelector('.queue-list');
-
-
-
-// const fetchMovies = async () => {
-//   try {
-//     const movieId = '';
-//     const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
-//     const movieToQueue = await fetch(
-//       `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`,
-//     );
-//     const movieToQueueJSON = await movieToQueue.json();
-//     console.log(movieToQueueJSON);
-//     return movieToQueueJSON;
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
-
-// const addToQueue = movie => {
-//   const imgUrl = 'https://image.tmdb.org/t/p/w500';
-//   const usersMovie = `
-//   <div>
-//       <img src="${imgUrl}${movie.poster_path}" loading="lazy" 
-//       />
-//       <div>
-//       <p>${movie.title}</p>
-//       <p> ${movie.genre_ids
-//         .map(genre => genre.name)
-//         .slice(0, 3)
-//         .join(', ')} | ${movie.release_date}</p>                   
-//       </div>
-//   </div>`;
-//   queueList.innerHTML = usersMovie;
-// };
-
-// // queueButton.addEventListener('click', () => {
-// //   localStorage.setItem('queueElements', addToQueue);
-// // });
-
-// // userQueue.addEventListener('click', () => {
-// //   const queueElement = localStorage.getItem('queueElements');
-// //   const queueItem = document.createElement('li');
-// //   queueItem.textContent = queueElement;
-// //   queueList.appendChild(queueItem);
-// // });
+    
+  })
