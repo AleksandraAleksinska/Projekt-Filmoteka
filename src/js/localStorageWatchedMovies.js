24753e2d9c03f8f554422e5d026734050ffa8055
MateuszBieda <mateusbieda@gmail.com>
2023-04-25 00:37:34 +0200

    pagination_function

--- src/js/localStorageWatchedMovies.js
+++ src/js/localStorageWatchedMovies.js
@@ -1,11 +1,11 @@
-import selectedMovie from './modal';
+// import selectedMovie from './modal';
 
-export { saveData };
-export { addToLocalStorage };
+// export { saveData };
+// export { addToLocalStorage };
 
-const btnWatched = document.querySelector('#add-to-watched');
+// const btnWatched = document.querySelector('#add-to-watched');
 
-btnWatched.addEventListener('click', addToLocalStorage);
+// btnWatched.addEventListener('click', addToLocalStorage);
 
 // function saveData(selectedMovieData) {
 
@@ -13,18 +13,18 @@ btnWatched.addEventListener('click', addToLocalStorage);
 //     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(movieDetails));
 // }
 
-function addToLocalStorage(movie) {
-  const btnWatched = document.querySelector('#add-to-watched');
-  console.log('hello');
-  const statusWatched = localStorage.getItem('watched');
+// function addToLocalStorage(movie) {
+//   const btnWatched = document.querySelector('#add-to-watched');
+//   console.log('hello');
+//   const statusWatched = localStorage.getItem('watched');
 
-  if (!statusWatched) {
-    btnWatched.addEventListener('click', addWatched);
-  }
+//   if (!statusWatched) {
+//     btnWatched.addEventListener('click', addWatched);
+//   }
 
-  function addWatched(e) {
-    if (e.target.textContent === 'add to watched') {
-      localStorage.setItem('watched', JSON.stringify([movie]));
-    }
-  }
-}
+//   function addWatched(e) {
+//     if (e.target.textContent === 'add to watched') {
+//       localStorage.setItem('watched', JSON.stringify([movie]));
+//     }
+//   }
+// }
