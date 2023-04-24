5bdfa3d2f7c440b360b549db33a46f28a242b06e
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2023-04-24 19:25:50 +0200

    local-storage__queue--new update

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,26 +1,29 @@
 const movieCard = document.querySelector('.modal__movie');
 
-const getSelectedMovieDetails = async (id) => {
-    try {
-        const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
-        const selectedMovieFetch = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`);
-        const selectedMovieData = await selectedMovieFetch.json();
-        return selectedMovieData;      
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+const getSelectedMovieDetails = async id => {
+  try {
+    const API_KEY = 'ac3e035161883f7175e5be9954a0068d';
+    const selectedMovieFetch = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`,
+    );
+    const selectedMovieData = await selectedMovieFetch.json();
+    return selectedMovieData;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const renderSelectedMovieDetails = (movie) => {
-    const imgUrl = 'https://image.tmdb.org/t/p/w500';
-    const selectedMovie =
-        `<img class="modal__img" src="${imgUrl}${movie.poster_path}"></img>
+const renderSelectedMovieDetails = movie => {
+  const imgUrl = 'https://image.tmdb.org/t/p/w500';
+  const selectedMovie = `<img class="modal__img" src="${imgUrl}${movie.poster_path}"></img>
         <div class="modal__wrapper">
             <h2 class="modal__title uppercase"> ${movie.title}</h2>
                 <ul class="modal__info">
                     <li class="modal__info-item">
                         <p class="modal__info-key">Vote / Votes</p>
-                        <p class="modal__info-number"><p class="rating-frame">${movie.vote_average.toFixed(1)}</p>&nbsp/&nbsp<p class="rating-frame--gray">${movie.vote_count}</p></p>
+                        <p class="modal__info-number"><p class="rating-frame">${movie.vote_average.toFixed(
+                          1,
+                        )}</p>&nbsp/&nbsp<p class="rating-frame--gray">${movie.vote_count}</p></p>
                     </li>
                     <li class="modal__info-item">
                         <p class="modal__info-key">Popularity</p>
@@ -32,7 +35,10 @@ const renderSelectedMovieDetails = (movie) => {
                    </li>
                     <li class="modal__info-item">
                        <p class="modal__info-key">Genre</p>
-                       <p class="modal__info-value">${movie.genres.map(genre => genre.name).slice(0,3).join(', ')}</p>
+                       <p class="modal__info-value">${movie.genres
+                         .map(genre => genre.name)
+                         .slice(0, 3)
+                         .join(', ')}</p>
                    </li>
                 </ul>
             <div class="modal__overview">
@@ -43,18 +49,17 @@ const renderSelectedMovieDetails = (movie) => {
                 <button id="add-to-watched" class="button button--accent">add to watched</button>
                 <button id="add-to-queue" class="button button--queue">add to queue</button>
         </div>
-        </div>`
+        </div>`;
 
-    movieCard.innerHTML = selectedMovie;
+  movieCard.innerHTML = selectedMovie;
 
-    const btnAddToQueue = document.querySelector('.button--queue');
-    btnAddToQueue.addEventListener('click', () => {
-        let queueArray = [];
-        localStorage.setItem('queue-movie', JSON.stringify(movie));
-      })
+  const btnAddToQueue = document.querySelector('.button--queue');
+  btnAddToQueue.addEventListener('click', () => {
+    const currentQueue = JSON.parse(localStorage.getItem('queue-movie')) || [];
+    currentQueue.push(movie);
+    localStorage.setItem('queue-movie', JSON.stringify(currentQueue));
+  });
+};
 
-    
-}
-
-const selectedMovie = { getSelectedMovieDetails, renderSelectedMovieDetails }
+const selectedMovie = { getSelectedMovieDetails, renderSelectedMovieDetails };
 export default selectedMovie;
