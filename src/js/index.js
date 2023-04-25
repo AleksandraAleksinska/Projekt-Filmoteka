5089ebfb4429a71617f2adfb44a0f1dfaa37a783
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-04-25 14:05:12 +0200

    sprzątanie kodu, drobne dodatki

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,5 @@ import { getMoviesbyKeyword } from './fetchMoviesByKeyword';
 import selectedMovie from './modal';
 import pagination from './pagination';
 import toggleModal from './toggleModal';
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 import './local-storage__queue';
 import './local-storage__watched';
--- src/js/main-page-trending.js
+++ src/js/main-page-trending.js
@@ -39,7 +39,7 @@ const renderTrendingMovies = response => {
       return `
         <li data-id=${id}>
         <div class="movie-card card-hover">
-            <img class="movie-card__img" src="${imgUrl}${poster_path}" loading="lazy" 
+            <img class="movie-card__img" src="${poster_path ? imgUrl+poster_path :'https://upload.wikimedia.org/wikipedia/commons/6/62/%22No_Image%22_placeholder.png' }" loading="lazy" 
             />
             <div class="movie-card__desc">
             <p class="movie-card__title">${title}</p>
