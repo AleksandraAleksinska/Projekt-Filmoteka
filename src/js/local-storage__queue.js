5705bf289d5f8ec5a2c6f19fece9693be698780d
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-04-25 16:57:41 +0200

    poprawki błędu w library

--- src/js/local-storage__queue.js
+++ src/js/local-storage__queue.js
@@ -7,58 +7,66 @@ const parsedMovies = JSON.parse(savedMovies);
 
 if(queueMoviesDOM) {
 
-const renderQueueMovie = parsedMovies.forEach((movie) => {
+if(!parsedMovies) {
+  console.log('You don’t have any video in queue')
+} else {
+
+  const renderQueueMovie = parsedMovies.forEach((movie) => {
   
-  const imgUrl = 'https://image.tmdb.org/t/p/w500';
-  const releaseYear = movie.release_date.slice(0, 4);
-  const markup = `
-        <li data-id=${movie.id}>
-        <div class="movie-card card-hover">
-            <img class="movie-card__img" src="${movie.poster_path ? imgUrl+movie.poster_path :'https://upload.wikimedia.org/wikipedia/commons/6/62/%22No_Image%22_placeholder.png' }" loading="lazy" 
-            />
-            <div class="movie-card__desc">
-            <p class="movie-card__title">${movie.title}</p>
-            <p class="movie-card__info"> ${movie.genres.map(genre => genre.name).slice(0,3).join(', ')} | ${releaseYear}</p>                     
-            </div>
-        </div>
-        </li>
-        `;
+    const imgUrl = 'https://image.tmdb.org/t/p/w500'; 
+    const releaseYear = movie.release_date.slice(0, 4);
+    const markup = `
+          <li data-id=${movie.id}>
+          <div class="movie-card card-hover">
+              <img class="movie-card__img" src="${movie.poster_path ? imgUrl+movie.poster_path :'https://upload.wikimedia.org/wikipedia/commons/6/62/%22No_Image%22_placeholder.png' }" loading="lazy" 
+              />
+              <div class="movie-card__desc">
+              <p class="movie-card__title">${movie.title}</p>
+              <p class="movie-card__info"> ${movie.genres.map(genre => genre.name).slice(0,3).join(', ')} | ${releaseYear}</p>                     
+              </div>
+          </div>
+          </li>
+          `;
+      
+      
+        queueMoviesDOM.insertAdjacentHTML('beforeend', markup);
+        const movieList = document.querySelectorAll('li');
+       movieList.forEach(movieListItem => {
+  
+        movieListItem.addEventListener('click', () => {
+          const movieId = movieListItem.dataset.id;
+          localStorage.setItem('movie-id', movieId);
+          document.querySelector('.backdrop').classList.remove('is-hidden');
+         
+          
+          selectedMovie.getSelectedMovieDetails(movieId)
+          .then((movie) => selectedMovie.renderSelectedMovieDetails(movie))
+          .catch((error) => console.log(error))       
+         
+        } )
+       }) 
+  
+      const libBtnQueue = document.querySelector('.active-button');
+      const libBtnWatched = document.querySelector('.btn-watched');
+  
+      libBtnWatched.addEventListener('click', () => {
+        queueMoviesDOM.classList.add('d-none');
+        watchedMoviesDOM.classList.remove('d-none');
+        libBtnWatched.classList.add('active-button');
+        libBtnQueue.classList.remove('active-button'); 
     
+        })
+        libBtnQueue.addEventListener('click', () => {
+        watchedMoviesDOM.classList.add('d-none');
+        queueMoviesDOM.classList.remove('d-none');      
+        libBtnWatched.classList.remove('active-button');
+        libBtnQueue.classList.add('active-button'); 
     
-      queueMoviesDOM.insertAdjacentHTML('beforeend', markup);
-      const movieList = document.querySelectorAll('li');
-     movieList.forEach(movieListItem => {
-
-      movieListItem.addEventListener('click', () => {
-        const movieId = movieListItem.dataset.id;
-        localStorage.setItem('movie-id', movieId);
-        document.querySelector('.backdrop').classList.remove('is-hidden');
-       
-        
-        selectedMovie.getSelectedMovieDetails(movieId)
-        .then((movie) => selectedMovie.renderSelectedMovieDetails(movie))
-        .catch((error) => console.log(error))       
-       
-      } )
-     }) 
-
-    const libBtnQueue = document.querySelector('.active-button');
-    const libBtnWatched = document.querySelector('.btn-watched');
-
-    libBtnWatched.addEventListener('click', () => {
-      queueMoviesDOM.classList.add('d-none');
-      watchedMoviesDOM.classList.remove('d-none');
-      libBtnWatched.classList.add('active-button');
-      libBtnQueue.classList.remove('active-button'); 
+        })
   
-      })
-      libBtnQueue.addEventListener('click', () => {
-      watchedMoviesDOM.classList.add('d-none');
-      queueMoviesDOM.classList.remove('d-none');      
-      libBtnWatched.classList.remove('active-button');
-      libBtnQueue.classList.add('active-button'); 
-  
-      })
+    })
 
-  })
+}
+
+
 }
--- src/js/local-storage__watched.js
+++ src/js/local-storage__watched.js
@@ -7,58 +7,66 @@ const parsedMovies = JSON.parse(savedMovies);
 
 if(watchedMoviesDOM) {
 
-const renderQueueMovie = parsedMovies.forEach((movie) => {
+  if(!parsedMovies) {
+    console.log('You haven’t watched any video')
+  } else {
+
+    const renderQueueMovie = parsedMovies.forEach((movie) => {
   
-  const imgUrl = 'https://image.tmdb.org/t/p/w500';
-  const releaseYear = movie.release_date.slice(0, 4);
-  const markup = `
-        <li data-id=${movie.id}>
-        <div class="movie-card card-hover">
-            <img class="movie-card__img" src="${movie.poster_path ? imgUrl+movie.poster_path :'https://upload.wikimedia.org/wikipedia/commons/6/62/%22No_Image%22_placeholder.png' }" loading="lazy" 
-            />
-            <div class="movie-card__desc">
-            <p class="movie-card__title">${movie.title}</p>
-            <p class="movie-card__info"> ${movie.genres.map(genre => genre.name).slice(0,3).join(', ')} | ${releaseYear}</p>                     
+      const imgUrl = 'https://image.tmdb.org/t/p/w500';
+      const releaseYear = movie.release_date.slice(0, 4);
+      const markup = `
+            <li data-id=${movie.id}>
+            <div class="movie-card card-hover">
+                <img class="movie-card__img" src="${movie.poster_path ? imgUrl+movie.poster_path :'https://upload.wikimedia.org/wikipedia/commons/6/62/%22No_Image%22_placeholder.png' }" loading="lazy" 
+                />
+                <div class="movie-card__desc">
+                <p class="movie-card__title">${movie.title}</p>
+                <p class="movie-card__info"> ${movie.genres.map(genre => genre.name).slice(0,3).join(', ')} | ${releaseYear}</p>                     
+                </div>
             </div>
-        </div>
-        </li>
-        `;
-    
-    
-      watchedMoviesDOM.insertAdjacentHTML('beforeend', markup);
-      const movieList = document.querySelectorAll('li');
-     movieList.forEach(movieListItem => {
-
-      movieListItem.addEventListener('click', () => {
-        const movieId = movieListItem.dataset.id;
-        localStorage.setItem('movie-id', movieId);
-        document.querySelector('.backdrop').classList.remove('is-hidden');
-       
+            </li>
+            `;
         
-        selectedMovie.getSelectedMovieDetails(movieId)
-        .then((movie) => selectedMovie.renderSelectedMovieDetails(movie))
-        .catch((error) => console.log(error))        
-       
-      } )
-     }) 
+        
+          watchedMoviesDOM.insertAdjacentHTML('beforeend', markup);
+          const movieList = document.querySelectorAll('li');
+         movieList.forEach(movieListItem => {
+    
+          movieListItem.addEventListener('click', () => {
+            const movieId = movieListItem.dataset.id;
+            localStorage.setItem('movie-id', movieId);
+            document.querySelector('.backdrop').classList.remove('is-hidden');
+           
+            
+            selectedMovie.getSelectedMovieDetails(movieId)
+            .then((movie) => selectedMovie.renderSelectedMovieDetails(movie))
+            .catch((error) => console.log(error))        
+           
+          } )
+         }) 
+    
+        const libBtnQueue = document.querySelector('.active-button');
+        const libBtnWatched = document.querySelector('.btn-watched');
+    
+        libBtnWatched.addEventListener('click', () => {
+        queueMoviesDOM.classList.add('d-none');
+        watchedMoviesDOM.classList.remove('d-none');
+        libBtnWatched.classList.add('active-button');
+        libBtnQueue.classList.remove('active-button'); 
+    
+        })
+        libBtnQueue.addEventListener('click', () => {
+        watchedMoviesDOM.classList.add('d-none');
+        queueMoviesDOM.classList.remove('d-none');
+        libBtnWatched.classList.remove('active-button');
+        libBtnQueue.classList.add('active-button'); 
+    
+        })
+    
+      })
 
-    const libBtnQueue = document.querySelector('.active-button');
-    const libBtnWatched = document.querySelector('.btn-watched');
+  }
 
-    libBtnWatched.addEventListener('click', () => {
-    queueMoviesDOM.classList.add('d-none');
-    watchedMoviesDOM.classList.remove('d-none');
-    libBtnWatched.classList.add('active-button');
-    libBtnQueue.classList.remove('active-button'); 
 
-    })
-    libBtnQueue.addEventListener('click', () => {
-    watchedMoviesDOM.classList.add('d-none');
-    queueMoviesDOM.classList.remove('d-none');
-    libBtnWatched.classList.remove('active-button');
-    libBtnQueue.classList.add('active-button'); 
-
-    })
-
-  })
 }
